2ecb3ebd68dbf9ba18a0947c0901b79fb50ec962
chikangxi <chikangxi@chikangxi.top>
2021-07-28 09:49:47 +0000

    change method to use bc

--- backend.cpp
+++ backend.cpp
@@ -2,21 +2,14 @@
 #include <sstream>
 #include <cstdio>
 #include <unistd.h>
+#include <sys/wait.h>
 
 Backend::Backend()
 {
-	pipe(bc_in_pipe);
-	pipe(bc_out_pipe);
-
-	bc_in = fdopen(bc_in_pipe[1], "w");
-	bc_in_fd = bc_in_pipe[1];
-	bc_out =fdopen(bc_out_pipe[0], "r");
-	bc_out_fd = bc_out_pipe[0];
 }
 
 Backend::~Backend()
 {
-	fprintf(bc_in, "quit\n");
 }
 
 CalcResult::CalcResult(std::string str) : is_error(false), str(str) {
@@ -36,36 +29,38 @@ CalcResult Backend::calc(std::string input) {
 }
 
 std::string Backend::calcNoExcp(std::string input) {
+	pipe(bc_in_pipe);
+	pipe(bc_out_pipe);
+
+	bc_in = fdopen(bc_in_pipe[1], "w");
+	bc_in_fd = bc_in_pipe[1];
+	bc_out =fdopen(bc_out_pipe[0], "r");
+	bc_out_fd = bc_out_pipe[0];
+
 	pid_t pid;
 	if (pid = fork()) {
 		fprintf(bc_in, "scale=10\n");
 		fprintf(bc_in, "%s\n", input.c_str());
 		fprintf(bc_in, "quit\n");
-		fflush(bc_in);
-		fd_set set;
-		FD_ZERO(&set);
-		FD_SET(bc_out_fd, &set);
-		struct timeval wait;
-		wait.tv_sec = 1;
-		wait.tv_usec = 0;
-		if (select(FD_SETSIZE, &set, NULL, NULL, &wait))
-		{
+		fclose(bc_in);
+		close(bc_in_fd);
+		waitpid(pid, NULL, 0);
+		close(bc_out_pipe[1]);
+
 			printf("bc has calculate result\n");
 			char buf[64];
-			fscanf(bc_out, "%s", buf);
+			int len = fread(buf, 1, 64-1, bc_out);
+			buf[len] = '\0';
 			std::string out(buf);
+
+			fclose(bc_out);
+			close(bc_out_fd);
 			return out;
-		}
-		else
-		{
-			printf("error while calculate\n");
-			return std::string("0");
-		}
 	}
 	else {
 		dup2(bc_in_pipe[0], STDIN_FILENO);
 		dup2(bc_out_pipe[1], STDOUT_FILENO);
-		execlp("bc", "bc", NULL);
+		execlp("bc", "bc", "-l", NULL);
 		printf("there is no program bc!!!\n");
 	}
 }
