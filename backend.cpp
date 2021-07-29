159641b02bad95fa8087ea5a52a4801f8f75981d
fpg2012 <fpg2012@foxmail.com>
2021-07-29 08:52:22 +0800

    format code

--- backend.cpp
+++ backend.cpp
@@ -29,39 +29,39 @@ CalcResult Backend::calc(std::string input) {
 }
 
 std::string Backend::calcNoExcp(std::string input) {
-	pipe(bc_in_pipe);
-	pipe(bc_out_pipe);
+    pipe(bc_in_pipe);
+    pipe(bc_out_pipe);
 
-	bc_in = fdopen(bc_in_pipe[1], "w");
-	bc_in_fd = bc_in_pipe[1];
-	bc_out =fdopen(bc_out_pipe[0], "r");
-	bc_out_fd = bc_out_pipe[0];
+    bc_in = fdopen(bc_in_pipe[1], "w");
+    bc_in_fd = bc_in_pipe[1];
+    bc_out =fdopen(bc_out_pipe[0], "r");
+    bc_out_fd = bc_out_pipe[0];
 
-	pid_t pid;
-	if (pid = fork()) {
-		fprintf(bc_in, "scale=10\n");
-		fprintf(bc_in, "%s\n", input.c_str());
-		fprintf(bc_in, "quit\n");
-		fclose(bc_in);
-		close(bc_in_fd);
-		waitpid(pid, NULL, 0);
-		close(bc_out_pipe[1]);
+    pid_t pid;
+    if (pid = fork()) {
+        fprintf(bc_in, "scale=10\n");
+        fprintf(bc_in, "%s\n", input.c_str());
+        fprintf(bc_in, "quit\n");
+        fclose(bc_in);
+        close(bc_in_fd);
+        waitpid(pid, NULL, 0);
+        close(bc_out_pipe[1]);
 
-			printf("bc has calculate result\n");
-			char buf[64];
-			int len = fread(buf, 1, 64-1, bc_out);
-			buf[len] = '\0';
-			std::string out(buf);
+        printf("bc has calculate result\n");
+        char buf[64];
+        int len = fread(buf, 1, 64 - 1, bc_out);
+        buf[len] = '\0';
+        std::string out(buf);
 
-			fclose(bc_out);
-			close(bc_out_fd);
-			return out;
-	}
-	else {
-		dup2(bc_in_pipe[0], STDIN_FILENO);
-		dup2(bc_out_pipe[1], STDOUT_FILENO);
-		execlp("bc", "bc", "-l", NULL);
-		printf("there is no program bc!!!\n");
-	}
+        fclose(bc_out);
+        close(bc_out_fd);
+        return out;
+    }
+    else {
+        dup2(bc_in_pipe[0], STDIN_FILENO);
+        dup2(bc_out_pipe[1], STDOUT_FILENO);
+        execlp("bc", "bc", "-l", NULL);
+        printf("there is no program bc!!!\n");
+    }
 }
 
